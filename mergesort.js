2bcabd5e7d97c65d73fbe404f28b1b9324ca7d05
Scott Techau <scotttechau@gmail.com>
2018-11-05 14:31:04 -0800

    completed merge function

--- mergesort.js
+++ mergesort.js
@@ -6,3 +6,21 @@ function split(array) {
   return [left, right];
 }
 
+function merge(arr1, arr2) {
+  let merged = [];
+  let i = 0, j = 0;
+  while(i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i++]);
+    } else {
+      merged.push(arr2[j++]);
+    }
+  }
+  if (i !== arr1.length) {
+    merged.push(...(arr1.slice(i)));
+  } else {
+    merged.push(...(arr2.slice(j)));
+  }
+
+  return merged;
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -3,6 +3,13 @@ describe('Split Array function', function() {
     expect( split([1,2,3,4,5,6]) ).toEqual( [ [1,2,3],[4,5,6] ] );
     expect( split([1,2,3,4,5]) ).toEqual( [ [1,2],[3,4,5] ] );
   });
+});
 
-
+describe('Merge function', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    expect( merge([1], [2]) ).toEqual([1,2]);
+    expect( merge([1,3], [2,4]) ).toEqual([1,2,3,4]);
+    expect( merge([5,6], [3,4]) ).toEqual([3,4,5,6]);
+    expect( merge([1,2], [5,8,10]) ).toEqual([1,2,5,8,10]);
+  });
 });
